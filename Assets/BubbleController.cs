b109fa4169ab70b18ec20f10749d9d288b17c288
dcprehn <dcprehn@mines.edu>
2024-02-17 20:05:07 -0700

    Finished mapping function from lat/long to pixels

--- Assets/BubbleController.cs
+++ Assets/BubbleController.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class BubbleController : MonoBehaviour
 {
-    private float scaleMin = 0.2f;
-    private float scaleMax = 0.3f;
-    private float scaleSpeed = 0.05f;
+    private float scaleMin = 15f;
+    private float scaleMax = 25f;
+    private float scaleSpeed = 1f;
     // Start is called before the first frame update
     private void Start()
     {
@@ -16,10 +16,10 @@ public class BubbleController : MonoBehaviour
     
     // Update is called every frame
     private void Update() {
-        // Bubble should oscillate between min and max scale
-        if (transform.localScale.x < scaleMin || transform.localScale.x > scaleMax) {
-            scaleSpeed *= -1;
-        }
-        transform.localScale += new Vector3(scaleSpeed, scaleSpeed, scaleSpeed) * Time.deltaTime;
+        // Bubble should oscillate between min and max scale TODO
+        // if (transform.localScale.x < scaleMin || transform.localScale.x > scaleMax) {
+        //     scaleSpeed *= -1;
+        // }
+        // transform.localScale += new Vector3(scaleSpeed, scaleSpeed, scaleSpeed) * Time.deltaTime;
     }
 }
--- Assets/BubbleSpawner.cs
+++ Assets/BubbleSpawner.cs
@@ -1,34 +1,57 @@
-using System.Buffers;
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using Unity.Mathematics;
+using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 
 public class BubbleSpawner : MonoBehaviour
 {
     public GameObject bubblePrefab;
     public GameObject map;
-    public int numBubblesToSpawn = 25;
-    public float spawnInterval = 1f;
+    public struct WorldCoords {
+        public float x;
+        public float y;
+    };
     // Start is called before the first frame update
     void Start()
     {
-        // Start spawning bubbles
-        InvokeRepeating("SpawnBubble", 2f, spawnInterval);
+        SpawnBubble();
     }
 
     void SpawnBubble() {
-        RectTransform mapRectTransform = map.GetComponent<RectTransform>();
-        float minX = mapRectTransform.position.x - mapRectTransform.rect.width / 2;
-        float maxX = minX + mapRectTransform.rect.width;
-        float minY = mapRectTransform.position.y - mapRectTransform.rect.height / 2;
-        float maxY = minY + mapRectTransform.rect.height;
-        Vector3 spawnPos = new Vector3(Random.Range(minX / 10, maxX / 10), Random.Range(minY / 10, maxY / 10), 0f);
-        GameObject newBubble = Instantiate(bubblePrefab, spawnPos, Quaternion.identity);
+        // For testing purposes, spawn a bubble at each intersection of prominent latitude/longitude lines
+        float[] longitudes = {-150f, -120f, -90f, -60f, -30f, 0f, 30f, 60f, 90f, 120f, 150f, 180f};
+        float[] latitudes = {-75f, -60f, -45f, -30f, -15f,  0f, 15f, 30f, 45f, 60f, 75f};
 
-        numBubblesToSpawn--;
-        if (numBubblesToSpawn <= 0) {
-            CancelInvoke("SpawnBubble");
+        foreach (float longitude in longitudes) {
+            foreach (float latitude in latitudes) {
+                WorldCoords coord = convertToWorldCoords(latitude, longitude);
+                Vector3 spawnPos = new Vector3(coord.x, coord.y, 0f);
+                GameObject newBubble = Instantiate(bubblePrefab, spawnPos, Quaternion.identity);
+            }
         }
+    }
+
+    private WorldCoords convertToWorldCoords(float latitude, float longitude) {
+        RectTransform mapRectTransform = map.GetComponent<RectTransform>();
+        // Define latitude/longitude bounds
+        const float MIN_LATITUDE = -71f;
+        const float MAX_LATITUDE = 71f;
+        const float MIN_LONGITUDE = -180f;
+        const float MAX_LONGITUDE = 180f;
+        // Calculate the scaled latitude
+        float latitudeScale = Mathf.Log(Mathf.Tan(0.25f * Mathf.PI + 0.5f * Mathf.Deg2Rad * latitude));
+        float latitudeRange = (MAX_LATITUDE - MIN_LATITUDE) * Mathf.Deg2Rad;
+        // Calculate x and y pixels for given latitude/longitude
+        float xPixel = (longitude + MAX_LONGITUDE) * (mapRectTransform.rect.width / (MAX_LONGITUDE - MIN_LONGITUDE));
+        float yPixel = (mapRectTransform.rect.height / 2.0f) - (mapRectTransform.rect.height * latitudeScale / (2 * latitudeRange));
+        // Calculate world coords based on the pixels
+        WorldCoords converted = new WorldCoords
+        {
+            x = mapRectTransform.position.x + (xPixel - mapRectTransform.rect.width / 2.0f),
+            y = mapRectTransform.position.y - (yPixel - mapRectTransform.rect.height / 2.0f)
+        };
+
+        return converted;
     }
 
     // Update is called once per frame
